6bd9e2c6cc6401d9954de4544df1d3bdd44007e3
Kian <kian.morot@hotmail.com>
2020-09-09 16:16:32 -0700

    Started working on the filters for adoptions

--- app/public/wp-content/themes/mesmerize/page-adoptions.php
+++ app/public/wp-content/themes/mesmerize/page-adoptions.php
@@ -1,9 +1,180 @@
 <?php mesmerize_get_header(); ?>
+<style>
+	.filters ul{
+		list-style: none;
+		margin: 0;
+	}
+	.filters ul li{
+		text-align: left;
+	}
+	.filters input{
+		height: 1rem;
+	}
+	.filters label{
+		font-size: 1rem;
+
+	}
+	.scrollRadio{
+		height: 15rem;
+		overflow: scroll;
+	}
+</style>
 <div id='page-content' class="page-content">
 	<div class="<?php mesmerize_page_content_wrapper_class(); ?>">
 		<div class="flexbox">
 			<div class="col-3" style="width: 100%; margin-right: .5rem;">
-						<?php wp_nav_menu(array('theme_location' => 'filter-menu')); ?>
+				<!--<?php wp_nav_menu(array('theme_location' => 'filter-menu')); ?>-->
+				<div class="filters">
+					<h2>
+						Filters
+					</h2>
+					<ul>
+						<li>
+							<hr>
+							<h4>Breed</h4>
+							<ul class="scrollRadio">
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<hr>
+							<h4>Age</h4>
+							<ul>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<hr>
+							<h4>Gender</h4>
+							<ul>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<hr>
+							<h4>Size</h4>
+							<ul>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<hr>
+							<h4>Good With</h4>
+							<Ul>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+								<li>
+									<input type="radio" id="GoldenRetriever" name="gender" value="GoldenRetriever">
+									<label for="GoldenRetriever">Golden Retriever</label><br>
+								</li>
+							</Ul>
+						</li>
+						<hr>
+					</ul>
+					<button type="button" style="width: 100%;">APPLY</button>
+				</div>
 			</div>
 			<div class="col-9">
 				<div class="row spaced-cols content-center-sm" data-type="row">
@@ -88,4 +259,5 @@
 		</div>
 	</div>
 </div>
+
 <?php get_footer(); ?>
