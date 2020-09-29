a4dbe7c86ebeaafd6e1b1e21f622700abe18501b
Kian <kian.morot@hotmail.com>
2020-09-29 15:40:33 -0700

    Fixed recently viewed bug additionally adding adoption id at char 0 being viewed

--- app/public/wp-content/themes/mesmerize/page-animal.php
+++ app/public/wp-content/themes/mesmerize/page-animal.php
@@ -4,9 +4,8 @@
 <?php mesmerize_get_header(); ?>
 <div id='page-content' class="page-content">
 	<div class="<?php mesmerize_page_content_wrapper_class(); ?>">
-		<?php
+        <?php
 			if(is_array($_SESSION["recentlyViewed"])){
-				print_r($_GET["id"]);
 				if(!(in_array($_GET["id"],$_SESSION["recentlyViewed"]))){
 					if(count($_SESSION["recentlyViewed"]) == 15){
 						array_shift($_SESSION["recentlyViewed"]);
@@ -18,7 +17,7 @@
 				}
 			}
 			else{
-				$_SESSION["recentlyViewed"] = array($_GET["id"][0]);
+				$_SESSION["recentlyViewed"] = array($_GET["id"]);
 			}
 			global $wpdb;
 			$query = "SELECT * FROM adoptionss WHERE adoption_id = " . $_GET["id"];
