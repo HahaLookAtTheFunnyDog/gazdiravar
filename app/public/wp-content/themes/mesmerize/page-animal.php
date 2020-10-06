2d7946ef6a85b4edb25c010fb3dc2e8984926c16
Kian <kian.morot@hotmail.com>
2020-10-06 11:47:02 -0700

    Added layout for primary portion of animal page

--- app/public/wp-content/themes/mesmerize/page-animal.php
+++ app/public/wp-content/themes/mesmerize/page-animal.php
@@ -28,7 +28,25 @@
 			<div class="row no-gutters">
 				<div class="col-md-8 ">
 					<section class="aboutSection">
+						<div class="slideShowContainer">
+							<div class="slideContainer">
+								<div class="slide">
+								</div>
+							</div>
+							<div class="slidePrev">
+							</div>
+							<div class="slideNext">
+							</div>
+						</div>
+						<div class="textPart">
 						<h1><?php echo ucwords($dog->name); ?></h1>
+						<h3>Country</h3>
+						<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et ultricies elit. Praesent fermentum nunc at turpis tempus, vitae ultricies lectus blandit. Morbi ut luctus leo. Integer ornare fermentum urna, at efficitur turpis. Vivamus malesuada, nulla quis porttitor venenatis, justo neque luctus mauris, sit amet maximus velit mauris non turpis. Nulla efficitur est et pulvinar scelerisque. In sed tristique erat. Donec convallis leo sit amet ligula placerat, ut ornare nunc ornare. Morbi ultrices sem mauris, non varius tellus aliquet in. Vivamus vel gravida elit. Fusce eu suscipit tortor, eget laoreet mauris. Nam lorem libero, porta non aliquam et, congue ac tortor. Nullam vehicula facilisis nunc vel tempor. Maecenas sit amet venenatis massa, sit amet pulvinar massa. Etiam mattis nulla lorem, ac tempor orci pretium quis. Sed at risus molestie felis ullamcorper efficitur id sit amet turpis.
+
+Integer eget porta odio. Aenean finibus, nulla eu aliquam rhoncus, nulla lorem eleifend metus, ac laoreet velit arcu non eros. Vivamus finibus, felis vitae egestas luctus, arcu nisl cursus tortor, vel commodo felis nulla vel purus. Vivamus tincidunt libero tempor mauris iaculis finibus. Fusce pellentesque ultrices odio sit amet mollis. Morbi nisl velit, laoreet at pellentesque eu, dictum vitae mi. Etiam vel lectus vitae elit bibendum varius id vitae mauris. Fusce non molestie dolor.
+						</p>
+						</div>
 					</section>
 				</div>
 				<div class="col-md-3 col-md-offset-1">
