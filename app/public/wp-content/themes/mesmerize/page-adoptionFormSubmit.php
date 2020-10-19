307dc07a9eb45419d5d8c53fe6dba5381f58bbab
Kian <kian.morot@hotmail.com>
2020-10-19 13:52:40 -0700

    fixed not inserting messages bug

--- app/public/wp-content/themes/mesmerize/page-adoptionFormSubmit.php
+++ app/public/wp-content/themes/mesmerize/page-adoptionFormSubmit.php
@@ -3,7 +3,7 @@
     if(isset($_POST)){
         $response = array();
         $response["success"] = true;
-       $wpdb->insert("messages", array(
+        $wpdb->insert("messages", array(
             "first_name" => $_POST["fname"],
             "last_name" => $_POST["lname"],
             "email" => $_POST["emailAddr"]
