6d67df5d3f99d5b7adf903a313ed5ebb32afdf26
Darrell-David Madanay <darrellmadanay@gmail.com>
2020-11-24 14:26:37 -0500

    removed "powered by wordpress" copyright

--- app/public/wp-content/themes/mesmerize/template-parts/footer/footer.php
+++ app/public/wp-content/themes/mesmerize/template-parts/footer/footer.php
@@ -3,7 +3,7 @@
         <div class="gridContainer">
 	        <div class="row middle-xs footer-content-row">
 	            <div class="footer-content-col col-xs-12">
-	                    <?php echo mesmerize_get_footer_copyright(); ?>
+	                    <?php echo 'Add Copyright Here'; ?>
 	            </div>
 	        </div>
 	    </div>
